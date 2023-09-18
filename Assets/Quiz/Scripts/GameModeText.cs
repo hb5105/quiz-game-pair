81b79e83471f7d358f3c5cb3fe9b55f667fdf68b
Hritik Ajay Bansal <43716075+hb5105@users.noreply.github.com>
2023-09-18 12:31:13 -0700

    second commit

---
+++ Assets/Quiz/Scripts/GameModeText.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro; // Import the TextMeshPro namespace
+
+public class GameModeText : MonoBehaviour
+{
+    public TextMeshProUGUI gameModeText; // Reference to your TextMeshPro component
+
+    private float timer = 0f;
+    private float changeInterval = 5f;
+
+    void Start()
+    {
+        // Initialize the text
+        gameModeText.text = "Game Mode: ";
+        UpdateGameMode(RandomBool()); // Set an initial random value
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= changeInterval)
+        {
+            timer = 0f;
+            UpdateGameMode(RandomBool()); // Change the text to a random value
+        }
+    }
+
+    // You can update the text using a method like this
+    public void UpdateGameMode(bool mode)
+    {
+        gameModeText.text = mode.ToString().ToLower();
+    }
+
+    // Helper function to generate a random boolean value
+    private bool RandomBool()
+    {
+        return Random.Range(0, 2) == 0;
+    }
+}
